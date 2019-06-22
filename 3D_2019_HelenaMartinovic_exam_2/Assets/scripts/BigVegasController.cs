70d1a76487a2005c4c3eb14de6ebe2b2909a243a
hela <helena3dg@gmail.com>
2019-06-22 19:02:45 +0200

    first few sec

---
+++ 3D_2019_HelenaMartinovic_exam_2/Assets/scripts/BigVegasController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigVegasController : MonoBehaviour
+{
+
+    const float speed = 2f;
+
+    void Update()
+    {
+        var currentPosition = transform.position;
+        currentPosition.z += (speed * Time.deltaTime);
+        transform.position = currentPosition;
+    }
+}
