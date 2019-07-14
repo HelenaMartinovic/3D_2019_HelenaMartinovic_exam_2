7e044346dcf0da55db0ca8b795c846dc17ecf347
hela <helena3dg@gmail.com>
2019-07-15 00:22:26 +0200

    added play button

---
+++ 3D_2019_HelenaMartinovic_exam_2/Assets/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class PlayButton : MonoBehaviour
+{
+    public Button playButton;
+    public PlayableDirector pd;
+    public BigVegasController player;
+    public Canvas canvas;
+
+    public void PlayTimeline(){
+        pd.Play();
+        player.enabled = true;
+        canvas.gameObject.SetActive(false);
+    }
+}
